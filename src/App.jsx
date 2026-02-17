92238581255fa0005eb7eec9a7270c224573c816
Shoaib <mominshoaib4@gmail.com>
2026-02-17 15:27:06 +0530

    Update App.jsx
    reducing state management & identifying unnecessary state

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import GameBoard from "./Components/GameBoard"
 import Player from "./Components/Player"
 import Log from "./Components/Log"
 
-function App() {
-  const [activePlayer, setActivePlayer] = useState('X') //lifting state-up
-  const [gameTurns, setGameTurns] = useState([]);
+function derivedActivePlayer(gameTurns){
+let currentPlayer = 'X';
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setActivePlayer((currActivePlayer) => (currActivePlayer === "X" ? "O" : "X"))
-    setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
+      if (gameTurns.length > 0 && gameTurns[0].player === "X") {
         currentPlayer = 'O';
       }
+      return currentPlayer;
+}
+
+function App() {
+  // const [activePlayer, setActivePlayer] = useState('X') //lifting state-up
+  const [gameTurns, setGameTurns] = useState([]);
+  const activePlayer = derivedActivePlayer(gameTurns);
+  let currentPlayer = 'X';
+
+      if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+        currentPlayer = 'O';
+      }
+
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setActivePlayer((currActivePlayer) => (currActivePlayer === "X" ? "O" : "X"))
+    setGameTurns((prevTurns) => {
+      const currentPlayer = derivedActivePlayer(prevTurns)
 
       const updatedTurns = [{ square: { row: rowIndex, col: colIndex }, player: currentPlayer}, ...prevTurns ]
       return updatedTurns;
